32caa73383e7d11ef352bf9aa823f24be58e411a
Cody Johns <cjohns365@gmail.com>
2023-10-02 10:30:40 -0600

    Add execution of sql statement returned by chatgpt

--- src/main.py
+++ src/main.py
@@ -14,23 +14,32 @@ Respond only with a syntactically correct SQL query using the information given 
 """
 
 response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": sys},
-    {"role": "user", "content": msg}
-  ],
-  temperature=0,
-  max_tokens=256
+  	model="gpt-3.5-turbo",
+  	messages=[
+    	{"role": "system", "content": sys},
+    	{"role": "user", "content": msg}
+  	],
+  	temperature=0,
+  	max_tokens=256
 )
 
 sql = response['choices'][0]['message']['content']
 
 print(sql)
 
-#conn = sqlite3.connect("nba.db")
+try:
+	conn = sqlite3.connect("nba.db")
+	c = conn.cursor()
+	c.execute(sql)
+    
+	results = c.fetchall()
+    
+	for result in results:
+		print(result)
+    
+except sqlite3.Error as e:
+  print("Error executing query", e)
 
-#TODO: use openai's chatgpt 3.5 turbo api to construct SQL query based on question.
-
-#TODO: execute sql query and display results.
-
-#conn.close()
+finally:
+  if conn:
+    conn.close()
