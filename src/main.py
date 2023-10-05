19efdfb4c6bcb1c99aa0638cd40c0036eed7cd33
Daniel Kingery <danielk92057@gmail.com>
2023-10-05 12:22:11 -0600

    print formatting

--- src/main.py
+++ src/main.py
@@ -37,12 +37,13 @@ except Exception as e:
 
 sql = response['choices'][0]['message']['content']
 
-print(sql)
+print('Query:\n', sql)
 
 sql_val = sqlvalidator.parse(sql)
 if not sql_val.is_valid():
 	raise ValueError("Invalid SQL syntax generated. Try rephrasing your question.")
 
+print('Results')
 try:
 	conn = sqlite3.connect("nba.db")
 	c = conn.cursor()
